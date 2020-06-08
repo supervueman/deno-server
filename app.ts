844b74493a7749cebdf9c9a08138d972d4c87255
supervueman <supervueman@gmail.com>
2020-06-08 22:39:57 +0400

    Add crud

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import { UserI, CtxI } from './api/interfaces.ts';
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 const app = new Application();
 const router = new Router();
@@ -15,23 +16,49 @@ const users: UserI[] = [{
 router
 	.get('/api/users', ({ response }) => {
 		response.status = 200;
-		response.body = {
-			users
-		};
+		response.body = { users };
 	})
 	.get('/api/users/:id', ({ response, params }) => {
 		const user: UserI | undefined = users.find(u => u.id === params.id);
 
 		if (user) {
 			response.status = 200;
-			response.body = {
-				user
-			}
+			response.body = { user }
 		} else {
 			response.status = 404;
-			response.body = {
-				message: 'Not found'
-			}
+			response.body = { message: 'Not found' }
+		}
+	})
+	.post('/api/users', async ({ response, request }) => {
+		const body = await request.body();
+
+		if (!request.hasBody) {
+			response.status = 400;
+			response.body = { message: 'Bad request' }
+		} else {
+			const user: UserI = body.value;
+			const myUUID = v4.generate();
+
+			user.id = myUUID;
+
+			users.push(user);
+
+			response.status = 201;
+			response.body = { user }
+		}
+	})
+	.put('/api/users/:id', async ({ params, response, request }) => {
+		const user: UserI | undefined = users.find(u => u.id === params.id);
+
+		if (user) {
+			const body = await request.body();
+			user.name = body.value.name;
+
+			response.status = 200;
+			response.body = { user }
+		} else {
+			response.status = 404;
+			response.body = { message: 'Not found' }
 		}
 	});
 
